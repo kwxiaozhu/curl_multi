75e43d9d956fe2f84e016d0c4f5c9e96062aefe9
kwxiaozhu <kwxiaozhu@users.noreply.github.com>
2017-09-20 19:37:02 +0800

    fix cpu 100%

--- libs/class_curl_multi.php
+++ libs/class_curl_multi.php
@@ -16,6 +16,18 @@ class MultiHttpRequest {
     public $urls = array();
     public $curlopt_header = 0;
     public $method = "GET";
+    public $opt = array(
+            CURLINFO_HEADER_OUT => false,
+            CURLOPT_HEADER => false,
+            CURLOPT_CONNECTTIMEOUT => 10, 
+            CURLOPT_TIMEOUT => 30, 
+            CURLOPT_AUTOREFERER => true,
+            // more useragent http://www.useragentstring.com/
+            CURLOPT_USERAGENT => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS => 5
+            ); 
     
     function __construct($urls = false) {
         $this->urls = $urls;
@@ -58,26 +70,42 @@ class MultiHttpRequest {
 
      private function add_handle($handle, $url) {
          $curl = curl_init();
+	 $opt = $this->opt;
          curl_setopt($curl, CURLOPT_URL, $url);
-         
-         curl_setopt($curl, CURLOPT_HEADER, $this->curlopt_header);
-         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+         curl_setopt_array($curl, $opt);
+          /*curl_setopt($curl, CURLOPT_HEADER, $this->curlopt_header);
+         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);*/
          curl_multi_add_handle($handle, $curl);
          return $curl;
      }
 
      private function exec_handle($handle) {
-         $flag = null;
+         /**$flag = null;
          do {
             curl_multi_exec($handle, $flag);
-         } while ($flag > 0);
+         } while ($flag > 0);**/
+        $active = null;
+        // 执行批处理句柄
+        do {
+            $mrc = curl_multi_exec($handle, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+        while ($active && $mrc == CURLM_OK) {
+            if (curl_multi_select($handle) != -1) {
+            do {
+                $mrc = curl_multi_exec($handle, $active);
+            } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+        }
      }
      
      public function get_content($url){
-		$ch = curl_init();
-		curl_setopt ($ch, CURLOPT_URL, $url);
-		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT,10);
-		return curl_exec($ch);
+        $ch = curl_init();
+        $opt = $this->opt;
+        curl_setopt ($ch, CURLOPT_URL, $url);
+        curl_setopt_array($curl, $opt);
+        /**curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT,10);**/
+        return curl_exec($ch);
      }
 }
